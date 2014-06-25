db8a8019a72a2d59739e5385cfc7b5452ce0b3a7
ampaiva@gmail.com <ampaiva@gmail.com@3c5e7811-5ef6-123b-2b0e-89665a4429b1>
2014-06-25 13:34:30 +0000

    Initial import

---
+++ trunk/src/test/java/com/ampaiva/hlo/ParserTest.java
@@ -0,0 +1,26 @@
+package com.ampaiva.hlo;
+
+import static org.junit.Assert.assertNotNull;
+import japa.parser.ParseException;
+import japa.parser.ast.CompilationUnit;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class ParserTest {
+
+    @Test
+    public void testParserClass() throws ParseException, IOException {
+        File fileIn = Helper.createFile("src/test/resources", "com.ampaiva", "PointTestClass");
+
+        CompilationUnit cu = Parser.parserClass(fileIn);
+        assertNotNull(cu);
+        System.out.println(cu);
+        Parser.changeToPublic(cu);
+        Parser.inlineGetSet(cu);
+        System.out.println(cu);
+        Helper.writeFile(Helper.createFile("src/test/resources", "com.ampaiva.out", "PointTestClass"), cu.toString());
+    }
+}
