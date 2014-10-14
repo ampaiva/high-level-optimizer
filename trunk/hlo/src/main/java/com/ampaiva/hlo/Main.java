fbfb62d1cd3439613a5e0b7f40882c47eaa2c134
ampaiva@gmail.com <ampaiva@gmail.com@3c5e7811-5ef6-123b-2b0e-89665a4429b1>
2014-10-14 12:06:37 +0000

    Project as local variable

--- trunk/hlo/src/main/java/com/ampaiva/hlo/Main.java
+++ trunk/hlo/src/main/java/com/ampaiva/hlo/Main.java
@@ -11,8 +11,10 @@ import com.ampaiva.hlo.cm.ConcernMetric;
 import com.ampaiva.hlo.cm.MetricsColector;
 
 public class Main {
+    private static final String PROJECT_SRC = "../HW/src";
+
     public static void main(String[] args) throws ParseException, FileNotFoundException, IOException {
-        MetricsColector metricsColector = new MetricsColector().addFolder("../HW/src");
+        MetricsColector metricsColector = new MetricsColector().addFiles(PROJECT_SRC, ".*\\.java");
         TreeSet<String> set = new TreeSet<String>();
         for (Entry<String, ConcernMetric> entry : metricsColector.getMetrics().getHash().entrySet()) {
             String key = entry.getKey().split("\\.")[entry.getKey().split("\\.").length - 1];
