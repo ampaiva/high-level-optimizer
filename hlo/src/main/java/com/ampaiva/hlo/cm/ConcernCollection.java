63fcc36491eb09a04d39e4b323e569a1f7988f5c
ampaiva <ampaiva@gmail.com>
2015-03-16 15:54:15 +0000

    Handling more scenarios

--- hlo/src/main/java/com/ampaiva/hlo/cm/ConcernCollection.java
+++ hlo/src/main/java/com/ampaiva/hlo/cm/ConcernCollection.java
@@ -2,17 +2,23 @@ package com.ampaiva.hlo.cm;
 
 import japa.parser.ast.ImportDeclaration;
 import japa.parser.ast.body.ConstructorDeclaration;
+import japa.parser.ast.body.MethodDeclaration;
+import japa.parser.ast.body.VariableDeclarator;
 import japa.parser.ast.expr.Expression;
 import japa.parser.ast.expr.MethodCallExpr;
+import japa.parser.ast.expr.ObjectCreationExpr;
+import japa.parser.ast.expr.VariableDeclarationExpr;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ConcernCollection extends ConcernMetric {
 
     private static final String DOT = ".";
     private final List<List<String>> sequences = new ArrayList<List<String>>();
+    private final HashMap<String, String> variables = new HashMap<String, String>();
 
     public ConcernCollection(String key, InputStream source) {
         super(key, source);
@@ -23,33 +29,109 @@ public class ConcernCollection extends ConcernMetric {
         sequences.add(new ArrayList<String>());
     }
 
+    public void countMethodDeclaration(MethodDeclaration obj) {
+        sequences.add(new ArrayList<String>());
+    }
+
+    public void countObjectCreationExpr(ObjectCreationExpr obj) {
+        getNodes().add(getSource(), obj.getBeginLine(), obj.getBeginColumn(), obj.getEndLine(), obj.getEndColumn());
+        List<String> lastSequence = sequences.get(sequences.size() - 1);
+        String importStr = getImport(obj.getType().toString());
+        StringBuilder fullName = new StringBuilder();
+        if (importStr != null) {
+            fullName.append(importStr);
+        } else {
+            fullName.append(obj.getType().toString());
+        }
+        fullName.append(DOT);
+        fullName.append(obj.getType().getName());
+        lastSequence.add(fullName.toString());
+    }
+
     public void countMethodCallExpr(MethodCallExpr obj) {
         getNodes().add(getSource(), obj.getBeginLine(), obj.getBeginColumn(), obj.getEndLine(), obj.getEndColumn());
         List<String> lastSequence = sequences.get(sequences.size() - 1);
-        lastSequence.add(getFullName(obj));
+        lastSequence.add(getFullName(obj.getName(), obj.getScope()));
     }
 
-    private String getFullName(MethodCallExpr methodCall) {
+    public void countVariableDeclarationExpr(VariableDeclarationExpr obj) {
+        for (VariableDeclarator variable : obj.getVars()) {
+            variables.put(variable.getId().getName(), obj.getType().toString());
+        }
+    }
+
+    private String getStaticImport(Expression scope) {
+        List<ImportDeclaration> imports = cu.getImports();
+        if (imports == null) {
+            return null;
+        }
+        for (ImportDeclaration importDeclaration : imports) {
+            if (importDeclaration.isStatic() && importDeclaration.getName().toString().endsWith(DOT + scope)) {
+                StringBuilder fullName = new StringBuilder();
+                fullName.append(importDeclaration.getName().toString()
+                        .substring(0, importDeclaration.getName().toString().lastIndexOf(DOT + scope) + 1));
+                return fullName.toString();
+            }
+        }
+        return null;
+    }
+
+    private String getImport(String objName) {
+        List<ImportDeclaration> imports = cu.getImports();
+        if (imports == null) {
+            return null;
+        }
+        for (ImportDeclaration importDeclaration : imports) {
+            if (importDeclaration.getName().toString().endsWith(DOT + objName)) {
+                return importDeclaration.getName().toString();
+            }
+        }
+        return null;
+    }
+
+    private String getLocalMethodImport() {
         StringBuilder fullName = new StringBuilder();
-        String objName = methodCall.getName();
-        Expression scope = methodCall.getScope();
-        if (scope == null) {
+        if (cu.getPackage() != null) {
+            fullName.append(cu.getPackage().getName()).append(DOT);
+        }
+        fullName.append(cu.getTypes().get(0).getName());
+        return fullName.toString();
+    }
+
+    private String getVariableTypeImport(String objName, Expression scope) {
+        String clazz = variables.get(scope.toString());
+        String importStr = getImport(clazz);
+        if (importStr != null) {
+            StringBuilder fullName = new StringBuilder();
+            fullName.append(importStr);
+            return fullName.toString();
+        }
+        return clazz == null ? scope.toString() : clazz;
+    }
+
+    private String getFullName(String objName, Expression scope) {
+        StringBuilder fullName = new StringBuilder();
+        if (scope == null && cu.getTypes().get(0).getName().equals(objName)) {
             if (cu.getPackage() != null) {
                 fullName.append(cu.getPackage().getName()).append(DOT);
             }
             fullName.append(cu.getTypes().get(0).getName()).append(DOT);
         } else {
-            List<ImportDeclaration> imports = cu.getImports();
-            if (imports != null) {
-                for (ImportDeclaration importDeclaration : imports) {
-                    if (importDeclaration.getName().toString().endsWith(DOT + scope)) {
-                        fullName.append(importDeclaration.getName().toString()
-                                .substring(0, importDeclaration.getName().toString().lastIndexOf(DOT + scope) + 1));
-                        break;
-                    }
-                }
+            String importStr = getStaticImport(scope);
+            if (importStr == null && scope == null) {
+                importStr = getLocalMethodImport();
             }
-            fullName.append(scope).append(DOT);
+            if (importStr == null && scope != null) {
+                importStr = getVariableTypeImport(objName, scope);
+                scope = null;
+            }
+            if (importStr != null) {
+                fullName.append(importStr);
+            }
+            if (scope != null) {
+                fullName.append(scope);
+            }
+            fullName.append(DOT);
         }
         fullName.append(objName);
         return fullName.toString();
