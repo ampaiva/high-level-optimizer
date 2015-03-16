1c2c90f6b08e4935903dbe6c23818512a3cb22d1
ampaiva@gmail.com <ampaiva@gmail.com@3c5e7811-5ef6-123b-2b0e-89665a4429b1>
2015-03-16 15:54:15 +0000

    Handling more scenarios

--- trunk/hlo/src/test/java/com/ampaiva/hlo/cm/ConcernCollectionTest.java
+++ trunk/hlo/src/test/java/com/ampaiva/hlo/cm/ConcernCollectionTest.java
@@ -74,4 +74,79 @@ public class ConcernCollectionTest {
         assertEquals("java.lang.System.out.println", sequence0.get(1));
     }
 
+    @Test
+    public void testGetMetricInSimpleClassWithVariableDeclarationWithImport() throws ParseException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("package com.ampaiva.test;");
+        sb.append("import com.ampaiva.other.FooClass;");
+        sb.append("public class SimpleClass {\n");
+        sb.append("    public SimpleClass(){\n");
+        sb.append("       FooClass variable = new FooClass();\n");
+        sb.append("       variable.foo();\n");
+        sb.append("    }\n");
+        sb.append("void foo() {\n");
+        sb.append("    }\n");
+        sb.append("}");
+        ConcernCollection concernCollection = getLOCCBySource(sb.toString());
+        List<List<String>> sequences = concernCollection.getSequences();
+        assertNotNull(sequences);
+        assertEquals(1, sequences.size());
+        List<String> sequence0 = sequences.get(0);
+        assertEquals(2, sequence0.size());
+        assertEquals("com.ampaiva.other.FooClass.FooClass", sequence0.get(0));
+        assertEquals("com.ampaiva.other.FooClass.foo", sequence0.get(1));
+    }
+
+    @Test
+    public void testGetMetricInSimpleClassWithVariableDeclarationWithTwoImport() throws ParseException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("package com.ampaiva.test;");
+        sb.append("import com.ampaiva.other.FooClass1;");
+        sb.append("import com.ampaiva.other.FooClass2;");
+        sb.append("public class SimpleClass {\n");
+        sb.append("    public SimpleClass(){\n");
+        sb.append("       FooClass1 variable = new FooClass1();\n");
+        sb.append("       variable.foo();\n");
+        sb.append("    }\n");
+        sb.append("void foo() {\n");
+        sb.append("       FooClass2 variable = new FooClass2();\n");
+        sb.append("       variable.foo();\n");
+        sb.append("    }\n");
+        sb.append("}");
+        ConcernCollection concernCollection = getLOCCBySource(sb.toString());
+        List<List<String>> sequences = concernCollection.getSequences();
+        assertNotNull(sequences);
+        assertEquals(2, sequences.size());
+        List<String> sequence0 = sequences.get(0);
+        assertEquals(2, sequence0.size());
+        assertEquals("com.ampaiva.other.FooClass1.FooClass1", sequence0.get(0));
+        assertEquals("com.ampaiva.other.FooClass1.foo", sequence0.get(1));
+        List<String> sequence1 = sequences.get(1);
+        assertEquals(2, sequence1.size());
+        assertEquals("com.ampaiva.other.FooClass2.FooClass2", sequence1.get(0));
+        assertEquals("com.ampaiva.other.FooClass2.foo", sequence1.get(1));
+    }
+
+    @Test
+    public void testGetMetricInSimpleClassWithVariableDeclarationWithoutImport() throws ParseException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("package com.ampaiva.test;");
+        sb.append("public class SimpleClass {\n");
+        sb.append("    public SimpleClass(){\n");
+        sb.append("       com.ampaiva.other.FooClass variable = new com.ampaiva.other.FooClass();\n");
+        sb.append("       variable.foo();\n");
+        sb.append("    }\n");
+        sb.append("void foo() {\n");
+        sb.append("    }\n");
+        sb.append("}");
+        ConcernCollection concernCollection = getLOCCBySource(sb.toString());
+        List<List<String>> sequences = concernCollection.getSequences();
+        assertNotNull(sequences);
+        assertEquals(1, sequences.size());
+        List<String> sequence0 = sequences.get(0);
+        assertEquals(2, sequence0.size());
+        assertEquals("com.ampaiva.other.FooClass.FooClass", sequence0.get(0));
+        assertEquals("com.ampaiva.other.FooClass.foo", sequence0.get(1));
+    }
+
 }
